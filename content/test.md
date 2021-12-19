956330cd2840c2f199497815bcc648a299e6fbb0
TakushiTokuyama <sokoninoruna@gmail.com>
2021-12-19 02:02:01 +0000

    Tokus_Memo

---
+++ content/test.md
@@ -0,0 +1,12 @@
+Title: test
+Date: 2021-12-18
+Category: test
+Tags: test
+
+# test
+
+Hello
+
+```python
+print('Hello')
+```
---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+AUTHOR = 'Tokus'
+SITENAME = 'Tokus_Memo'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = 'ja'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('HatenaBlog', 'https://tokuty.hatenablog.com/'))
+
+# Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = './themes/pelican-svbhack'
